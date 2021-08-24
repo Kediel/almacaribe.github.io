c1d4377e2d4aba78417e96fc5122e1699d6538fa
Kediel <km5655@nyu.edu>
2021-08-23 21:54:19 -0400

    Deleted other 404 page and minor changes.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // footer year variable
 var d = new Date();
   var n = d.getFullYear();
-  document.getElementById("copyright").innerHTML = "&copy; Copyright " + n + ", Alma Caribe, LLC";
+  document.getElementById("copyright").innerHTML = "&copy; Copyright " + n + ", Alma Caribe, LLC. All rights reserved.";
 
 // fade when scrolling
   $(window).scroll(function(){
