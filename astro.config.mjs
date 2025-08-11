9dfe46dd539ba822b5ce881879cfabc96f4b2c24
Kediel <km5655@nyu.edu>
2025-08-11 19:10:13 -0400

    Troubleshooting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+build: {
+    assets: 'assets'
+  },
 export default defineConfig({
   site: 'https://almacaribe.co'
   integrations: [tailwind(), react()],
