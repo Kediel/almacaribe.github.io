3fae3c37abba6e3eeaa1d5fee30a4ec65dc45ad4
Kediel <km5655@nyu.edu>
2025-08-11 17:15:37 -0400

    Migrating to Astro for website.

---
+++ astro/alma/src/data/experience.ts
@@ -0,0 +1,41 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "May 2023 — Present",
+    title: "Cybersecurity Engineer",
+    company: "REDACTED",
+    companyUrl: "REDACTED",
+    description:
+      "Lead a 7+ team of engineers in cyber security capability development based on the Software Development Life Cycle and Model Based Engineering methodologies. Conducting Cyber Threat Analysis utilizing frameworks such as OWASP Top 10, SANS Top 25, MITRE Top 25, NIST RMF, and more to create actionable Cyber Threat Intelligence documentation and customer presentations.",
+    technologies: [
+      "Ansible",
+      "STRIDE",
+      "Gitlab",
+      "Python",
+      "Shell",
+      "Red Hat",
+    ],
+    logo: "/logos/dutchpet.jpeg",
+  },
+  {
+    dates: "May 2018 — May 2023",
+    title: "Network Security Engineer",
+    company: "REDACTED",
+    companyUrl: "REDACTED",
+    description:
+      "Configured and managed Cisco networking devices; including Integrated Services Routers, Firepower Management Centers, Catalyst Switches, Adapative Security Appliances, Web Security Appliance, Email Security Appliance, and Identity Services Engine. As well as being an AGILE Scrum Master, travelling to customer sites, documenting various network design documents and maintaning Diminished Manufacturer Supply.",
+    technologies: [
+      "Cisco IOS",
+      "Atlassian JIRA",
+      "Visio",
+      "Linux",
+      "Atlassian Bitbucket",
+      "Python",
+      "Shell Scripting",
+      "Wireshark",
+      "Routing protocols",
+    ],
+    logo: "REDACTED",
+  },
+];
---
+++ astro/alma/src/data/socials.ts
@@ -0,0 +1,33 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconMastadon from "../components/icons/mastadon.astro";
+import IconBlueSky from "../components/icons/bluesky.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/Kediel",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/Kediel/",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Mastadon",
+    url:"https://defcon.social/@k3ma5",
+    icon: IconMastadon,
+  },
+  {
+    name: "BlueSky",
+    url:"https:/bsky.app/profile/k3ma5",
+    icon: IconBlueSky,
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/k3ma85",
+    icon: IconTwitter,
+  },
+];
